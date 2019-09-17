8d65bb627aaa0693b46058f4595252c0087ad4c2
Jashele T <jashele@Jasheles-Air.domain>
2019-09-17 10:49:59 -0700

    Updates

---
+++ precache-manifest.00b11359ebe84679f5bce74b1965bc7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd006234289a02e193661d8963e2d898",
+    "url": "/jashele-tv/index.html"
+  },
+  {
+    "revision": "d942f4825021da6da3ad",
+    "url": "/jashele-tv/static/css/main.70d93641.chunk.css"
+  },
+  {
+    "revision": "8b9e511cd7dc3ad681b5",
+    "url": "/jashele-tv/static/js/2.ea655ae3.chunk.js"
+  },
+  {
+    "revision": "d942f4825021da6da3ad",
+    "url": "/jashele-tv/static/js/main.f067692e.chunk.js"
+  },
+  {
+    "revision": "10582a690e8692219c86",
+    "url": "/jashele-tv/static/js/runtime~main.2b522ce8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jasheletv/precache-manifest.b1a0bd50497409167d9df232384cd72e.js"
+  "/jashele-tv/precache-manifest.00b11359ebe84679f5bce74b1965bc7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jasheletv/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jashele-tv/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
