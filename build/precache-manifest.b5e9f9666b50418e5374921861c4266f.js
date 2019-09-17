17cd67c9277d7586438135285b8bb4c86872e8c0
Jashele T <jashele@Jasheles-Air.domain>
2019-09-17 11:01:22 -0700

    updated style on the nav

---
+++ build/precache-manifest.b5e9f9666b50418e5374921861c4266f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6377371e68786907ab2bd1e8424cf824",
+    "url": "/jashele-tv/index.html"
+  },
+  {
+    "revision": "911297cd5d517b44dc95",
+    "url": "/jashele-tv/static/css/main.3886ae60.chunk.css"
+  },
+  {
+    "revision": "8b9e511cd7dc3ad681b5",
+    "url": "/jashele-tv/static/js/2.ea655ae3.chunk.js"
+  },
+  {
+    "revision": "911297cd5d517b44dc95",
+    "url": "/jashele-tv/static/js/main.f067692e.chunk.js"
+  },
+  {
+    "revision": "10582a690e8692219c86",
+    "url": "/jashele-tv/static/js/runtime~main.2b522ce8.js"
+  }
+]);
