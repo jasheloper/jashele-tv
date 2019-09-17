8019ebb093a3a8c75f9b4146f25df53e4244b778
Jashele T <jashele@Jasheles-Air.domain>
2019-09-17 10:12:19 -0700

    Updates

---
+++ precache-manifest.c5afe52910a807e681cb04688b068f96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2ddfa5db3ac9600ff084a15daeb127",
+    "url": "/jashele-tv/index.html"
+  },
+  {
+    "revision": "ddb729c2b231639c1a83",
+    "url": "/jashele-tv/static/css/main.70d93641.chunk.css"
+  },
+  {
+    "revision": "8b9e511cd7dc3ad681b5",
+    "url": "/jashele-tv/static/js/2.ea655ae3.chunk.js"
+  },
+  {
+    "revision": "ddb729c2b231639c1a83",
+    "url": "/jashele-tv/static/js/main.684608f9.chunk.js"
+  },
+  {
+    "revision": "10582a690e8692219c86",
+    "url": "/jashele-tv/static/js/runtime~main.2b522ce8.js"
+  }
+]);
